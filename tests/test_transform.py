7941634b6cee25a2506bb4905a9bf4c7d71bbb07
Alejandro Brugarolas <abrugaro@redhat.com>
2023-09-25 11:54:56 +0200

    add rules transformation test
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

---
+++ tests/test_transform.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+import yaml
+from jsonschema.exceptions import ValidationError
+
+from utils import constants
+from jsonschema import validate
+
+
+# Polarion TC 376
+def test_transform_xml_rules_to_yaml():
+    kantra_path = os.getenv(constants.KANTRA_CLI_PATH)
+    custom_rule_path = os.path.join(os.getenv(constants.PROJECT_PATH), 'data/xml', 'javax-package-custom.windup.xml')
+    output_path = os.getenv(constants.REPORT_OUTPUT_PATH)
+
+    command = kantra_path + ' transform rules --input ' + custom_rule_path + ' --output ' + output_path
+
+    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+    yaml_path = output_path + "/01-javax-package-custom/javax-package-custom.windup.yaml"
+
+    assert os.path.exists(yaml_path)
+    with open(yaml_path) as file:
+        yaml_rule = file.read()
+
+    assert 'javax-package-00001' in yaml_rule
+
+    try:
+        validate(
+            yaml.load(yaml_rule, Loader=yaml.FullLoader),
+            yaml.load(constants.CUSTOM_RULE_YAML_SCHEMA, Loader=yaml.FullLoader)
+        )
+    except ValidationError as e:
+        assert True is False, 'The validation of the generated YAML rule failed. ' + e.message
--- utils/constants.py
+++ utils/constants.py
@@ -5,3 +5,47 @@ KANTRA_CLI_PATH = "KANTRA_CLI_PATH"
 REPORT_OUTPUT_PATH = "REPORT_OUTPUT_PATH"
 PROJECT_PATH = "PROJECT_PATH"
 
+# YAML RULE SCHEMA
+CUSTOM_RULE_YAML_SCHEMA = """
+type: array
+items:
+  type: object
+  properties:
+    category:
+      type: string
+    customVariables:
+      type: array
+      items:
+        type: object
+    description:
+      type: string
+    effort:
+      type: number
+    labels:
+      type: array
+      items:
+        type: string
+    links:
+      type: array
+      items:
+        type: object
+        properties:
+          title:
+            type: string
+          url:
+            type: string
+    message:
+      type: string
+    ruleID:
+      type: string
+    when:
+      type: object
+      properties:
+        java.referenced:
+          type: object
+          properties:
+            location:
+              type: string
+            pattern:
+              type: string
+"""
