fd1860c2e056ce33ba1ba2f955c1347d9ce60411
Alejandro Brugarolas <abrugaro@redhat.com>
2023-11-08 11:16:14 +0100

    adapt tests to the latest changes
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- utils/command.py
+++ utils/command.py
@@ -26,7 +26,7 @@ def build_analysis_command(binary_name, source, target, **kwargs):
 
     binary_path = os.path.join(os.getenv(constants.PROJECT_PATH), 'data/applications', binary_name)
 
-    command = kantra_path + ' analyze --input ' + binary_path + ' --output ' + report_path
+    command = kantra_path + ' analyze --overwrite --input ' + binary_path + ' --output ' + report_path
 
     if source:
         command += ' --source ' + source
