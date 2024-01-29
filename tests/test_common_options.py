0f93b09fcdb31dab6970ab4279f973d8a601ec63
Alejandro Brugarolas <abrugaro@redhat.com>
2024-01-29 17:39:51 +0100

    Automate sources and targets duplicates check
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- tests/test_common_options.py
+++ tests/test_common_options.py
@@ -15,6 +15,17 @@ def test_list_targets():
         assert i in output
 
 
+# Polarion TC MTA-472
+def test_list_targets_duplicates_bug_mta_2064():
+    kantra_path = os.getenv(constants.KANTRA_CLI_PATH)
+    command = kantra_path + ' analyze --list-targets'
+
+    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+
+    targets = str.split(output, "\n")
+    assert len(targets) == len(set(targets))
+
+
 # Polarion TC MTA-372
 def test_list_sources():
     kantra_path = os.getenv(constants.KANTRA_CLI_PATH)
@@ -24,3 +35,14 @@ def test_list_sources():
 
     for i in ['log4j', 'eap', 'springboot', 'openjdk11', 'java-ee', 'javaee', 'openshift', 'oraclejdk']:
         assert i in output
+
+
+# Polarion TC MTA-488
+def test_list_sources_duplicates():
+    kantra_path = os.getenv(constants.KANTRA_CLI_PATH)
+    command = kantra_path + ' analyze --list-sources'
+
+    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+
+    sources = str.split(output, "\n")
+    assert len(sources) == len(set(sources))
