f0236ad6a4d01f2d05f34a13d1bf2c4b9e8eef27
Alejandro Brugarolas <abrugaro@redhat.com>
2024-08-02 16:27:23 +0200

    fix pr
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- tests/analysis/dotnet/test_dotnet_analysis.py
+++ tests/analysis/dotnet/test_dotnet_analysis.py
@@ -5,8 +5,13 @@ from utils import constants
 from utils.command import build_analysis_command
 from utils.common import extract_zip_to_temp_dir
 
+
 # Polarion TC MTA-568
 def test_hello_world_analysis_with_rules(dotnet_analysis_data):
+    # Avoid running this test on Windows
+    if os.name == 'nt':
+        return
+
     application_data = dotnet_analysis_data["hello_world"]
     application_path = os.path.join(
         os.getenv(constants.PROJECT_PATH),
@@ -16,7 +21,6 @@ def test_hello_world_analysis_with_rules(dotnet_analysis_data):
     custom_rules_path = os.path.join(os.getenv(constants.PROJECT_PATH), 'data/yaml/dotnet/example_rules')
 
     with extract_zip_to_temp_dir(application_path) as tempdir:
-
         command = build_analysis_command(
             tempdir,
             "",
