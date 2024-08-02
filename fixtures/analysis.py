6abd8e40bf05d2470f55e9ad89537e1621f2e8a6
Alejandro Brugarolas <abrugaro@redhat.com>
2024-08-02 16:18:26 +0200

    add hello-world dotnet analysis test
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- fixtures/analysis.py
+++ fixtures/analysis.py
@@ -7,3 +7,10 @@ def analysis_data():
     with open('data/analysis.json', 'r') as file:
         json_list = json.load(file)
     return json_list
+
+
+@pytest.fixture(scope="session")
+def dotnet_analysis_data():
+    with open('data/dotnet_analysis.json', 'r') as file:
+        json_list = json.load(file)
+    return json_list
---
+++ tests/analysis/dotnet/test_dotnet_analysis.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+from utils import constants
+from utils.command import build_analysis_command
+from utils.common import extract_zip_to_temp_dir
+
+# Polarion TC MTA-568
+def test_hello_world_analysis_with_rules(dotnet_analysis_data):
+    application_data = dotnet_analysis_data["hello_world"]
+    application_path = os.path.join(
+        os.getenv(constants.PROJECT_PATH),
+        'data/applications',
+        application_data['file_name']
+    )
+    custom_rules_path = os.path.join(os.getenv(constants.PROJECT_PATH), 'data/yaml/dotnet/example_rules')
+
+    with extract_zip_to_temp_dir(application_path) as tempdir:
+
+        command = build_analysis_command(
+            tempdir,
+            "",
+            "",
+            **{'rules': custom_rules_path}
+        )
+
+        output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+
+        assert 'Static report created' in output
