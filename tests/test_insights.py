cb5b882f3cffbe3f118a46fe5ddde17374d945e1
Nandini Chandra <nachandr@redhat.com>
2024-08-16 09:58:48 -0500

    Initial commit for Insights test
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

---
+++ tests/test_insights.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+from utils import constants
+from utils.command import build_analysis_command
+from utils.report import assert_story_points_from_report_file, get_json_from_report_output_file
+
+# Polarion TC 374
+def test_insights(analysis_data):
+    application_data = analysis_data['jee_example_app']
+    custom_rule_path = os.path.join(os.getenv(constants.PROJECT_PATH), 'data/xml', 'weblogic-custom.windup.yaml')
+
+    command = build_analysis_command(
+        application_data['file_name'],
+        application_data['source'],
+        application_data['target'],
+        **{'rules': custom_rule_path}
+    )
+
+    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+
+    assert 'generating static report' in output
+
+    report_data = get_json_from_report_output_file()
+
+    ruleset = next((item for item in report_data['rulesets'] if item.get('description') == 'temp ruleset'), None)
+
+    assert ruleset is not None, "Ruleset property not found in output"
+    assert len(ruleset.get('skipped', [])) == 0, "Custom Rule was skipped"
+    assert len(ruleset.get('unmatched', [])) == 0, "Custom Rule was unmatched"
