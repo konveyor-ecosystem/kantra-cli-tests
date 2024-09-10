8de6ccb563876a2a415a631d7ded566277b28b27
kpunwatk <kpunwatk@redhat.com>
2024-09-10 22:17:32 +0530

    [RFR] Automate Java Provider (#25)

---
+++ tests/analysis/java/test_selective_java_provider.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+from utils.command import build_analysis_command
+from utils.report import assert_story_points_from_report_file
+
+# Polarion TC MTA-536
+def test_java_provider_analysis_with_app(analysis_data):
+
+    application_data = analysis_data['jee_example_app']
+    command = build_analysis_command(
+            application_data['file_name'],
+            "",
+            "eap8",
+            **{'provider': "java"}
+        )
+
+    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+
+    assert 'Static report created' in output
+    
+    assert_story_points_from_report_file()
