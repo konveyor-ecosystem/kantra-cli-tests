a298bcfb796ae4775e13225ecf982f6458f8d2ca
Nandini Chandra <nachandr@redhat.com>
2024-09-11 23:14:47 -0500

    Generate Insights by analyzing app with source-only mode
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- tests/test_insights.py
+++ tests/test_insights.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import pytest
 
 from utils import constants
 from utils.command import build_analysis_command
@@ -21,17 +22,26 @@ def test_insights_binary_app(analysis_data):
     assert_insights_from_report_file()
 
 # Polarion TC 3504, 3505, 3506
-def test_custom_rules(analysis_data):
+@pytest.mark.parametrize('analysis_mode', ["source-only", "full"])
+def test_custom_rules(analysis_data, analysis_mode):
     application_data = analysis_data['tackle-testapp-project']
     custom_rule_path = os.path.join(os.getenv(constants.PROJECT_PATH), 'data/yaml', 'custom_rule_insights.yaml')
  
-    command = build_analysis_command(
-        application_data['file_name'],
-        application_data['source'],
-        application_data['target'],
-        **{'rules': custom_rule_path}
-    )
- 
+    if analysis_mode == 'source-only':
+        command = build_analysis_command(
+            application_data['file_name'],
+            application_data['source'],
+            application_data['target'],
+            **{'rules': custom_rule_path},
+            **{'mode': 'source-only'},
+        )
+    else:
+        command = build_analysis_command(
+            application_data['file_name'],
+            application_data['source'],
+            application_data['target'],
+            **{'rules': custom_rule_path}
+        )
     output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
     assert 'generating static report' in output
  
