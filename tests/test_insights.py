6a22910e8c0d1f0bf13c4f58110084366de23771
Nandini Chandra <nachandr@redhat.com>
2024-09-13 15:57:56 -0500

    Formatting
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- tests/test_insights.py
+++ tests/test_insights.py
@@ -9,14 +9,15 @@ from utils.report import assert_insights_from_report_file, get_json_from_report_
 # Polarion TC 598
 def test_insights_binary_app(analysis_data):
     application_data = analysis_data['jee_example_app']
- 
+
     command = build_analysis_command(
         application_data['file_name'],
         application_data['source'],
         application_data['target']
     )
 
-    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE,
+        encoding='utf-8').stdout
 
     assert 'generating static report' in output
     assert_insights_from_report_file()
@@ -25,8 +26,9 @@ def test_insights_binary_app(analysis_data):
 @pytest.mark.parametrize('analysis_mode', ["source-only", "full"])
 def test_custom_rules(analysis_data, analysis_mode):
     application_data = analysis_data['tackle-testapp-project']
-    custom_rule_path = os.path.join(os.getenv(constants.PROJECT_PATH), 'data/yaml', 'custom_rule_insights.yaml')
- 
+    custom_rule_path = os.path.join(os.getenv(constants.PROJECT_PATH), 'data/yaml',
+        'custom_rule_insights.yaml')
+
     if analysis_mode == 'source-only':
         command = build_analysis_command(
             application_data['file_name'],
@@ -42,16 +44,17 @@ def test_custom_rules(analysis_data, analysis_mode):
             application_data['target'],
             **{'rules': custom_rule_path}
         )
-    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE,
+        encoding='utf-8').stdout
     assert 'generating static report' in output
- 
+
     report_data = get_json_from_report_output_file()
     for rule in report_data['rulesets']:
         insights = rule.get('insights', {})
- 
+
         for insight in insights.values():
             if rule['name'] == 'custom-ruleset':
-                if insight['description'] in ('Properties file (Insights TC0)', 'Properties file (Insights TC1)',
-                    'Properties file (Insights TC2)'):
+                if insight['description'] in ('Properties file (Insights TC0)',
+                    'Properties file (Insights TC1)', 'Properties file (Insights TC2)'):
                     # Assert insight occurrence is > 0 for each insight
                     assert len(insight['incidents']) > 0, "No insights were generated"
