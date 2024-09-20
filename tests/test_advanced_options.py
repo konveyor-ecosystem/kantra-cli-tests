0930938b99a1e49c8e724fc0a581c118ed2fc21c
Igor Braginsky <ibragins@redhat.com>
2024-09-20 11:08:11 +0300

    Implemented validation of MTA-3779 (#37)
    * Implemented validation of MTA-3779
    Link to the bug: https://issues.redhat.com/browse/MTA-3779
    Added tackle-testapp-public to the apps list
    Added function to read and to write to XML
    
    * Added few fixes by Alex's request
    
    * Update utils/constants.py
    
    ---------
    
    Co-authored-by: Alejandro Brugarolas <117646518+abrugaro@users.noreply.github.com>

--- tests/test_advanced_options.py
+++ tests/test_advanced_options.py
@@ -3,6 +3,7 @@ import subprocess
 
 from utils import constants
 from utils.command import build_analysis_command
+from utils.manage_maven_credentials import manage_credentials_in_maven_xml
 from utils.report import assert_story_points_from_report_file, get_json_from_report_output_file, clearReportDir
 
 
@@ -77,3 +78,24 @@ def test_bulk_analysis(analysis_data):
         assert len(current_report['depItems']) >= 0, "No dependencies were found"
         violations = [item for item in current_report['rulesets'] if item.get('violations')]
         assert len(violations) > 0, "No issues were found";
+
+# Validation for Jira ticket MTA-3779
+def test_analysis_of_private_repo(analysis_data):
+    application_data = analysis_data['tackle-testapp-public']
+    custom_maven_settings = os.path.join(os.getenv(constants.PROJECT_PATH), 'data/xml', 'tackle-testapp-public-settings.xml')
+    manage_credentials_in_maven_xml(custom_maven_settings)
+    command = build_analysis_command(
+        application_data['file_name'],
+        application_data['source'],
+        application_data['target'],
+        **{'maven-settings': custom_maven_settings}
+    )
+    output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+    assert 'generating static report' in output
+
+    report_data = get_json_from_report_output_file(False)
+    assert len(report_data[0]['depItems']) >= 0, "No dependencies were found"
+    violations = [item for item in report_data[0]['rulesets'] if item.get('violations')]
+    assert len(violations) > 1, "Expected issues are missing";
+
+    manage_credentials_in_maven_xml(custom_maven_settings, True)
