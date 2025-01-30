0c773fda0cdd91acebe8f77ab6275e536f34a664
Marek Aufart <maufart@redhat.com>
2025-01-30 17:45:22 +0100

    Checkpoint
    Signed-off-by: Marek Aufart <maufart@redhat.com>

--- utils/output.py
+++ utils/output.py
@@ -18,7 +18,7 @@ def assert_analysis_output_violations(expected_output_dir, output_dir, input_roo
 
     expected_output, expected_output_path = dict(), os.path.join(expected_output_dir, "output.yaml")
     got_output, got_output_path = get_dict_from_output_file("output.yaml", dir=output_dir)
-    got_output_normalized_path = got_output_path + ".normalized"
+    got_output_normalized_path = got_output_path + ".normalized.yaml"
     # create a preprocessed/normalized outfile file to allow its comparision across platforms and setups
     with open(got_output_normalized_path, 'w') as f:
             yaml.dump(normalize_output(got_output, input_root_path), f)
