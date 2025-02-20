0350c1cd97365633544d4f64c91a3e55cc62470a
Igor Braginsky <ibragins@redhat.com>
2025-02-20 12:24:50 +0200

    Forced to run containerless only on Windows
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from dotenv import load_dotenv
 
@@ -10,3 +11,10 @@ pytest_plugins = [
 @pytest.fixture(scope="session", autouse=True)
 def load_env():
     load_dotenv()
+
+def pytest_runtest_setup(item):
+    item.start_time = time.perf_counter()
+
+def pytest_runtest_teardown(item):
+    duration = time.perf_counter() - item.start_time
+    print(f"\nTest {item.name} took {duration:.4f} seconds")
