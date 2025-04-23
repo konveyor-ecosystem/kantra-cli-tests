dcbf482e978ead81205b5e66d117651a4d7bdbb4
Alejandro Brugarolas <abrugaro@redhat.com>
2025-04-23 12:18:59 +0200

    remove bug number
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- tests/analysis/java/test_java_analysis.py
+++ tests/analysis/java/test_java_analysis.py
@@ -6,7 +6,7 @@ import pytest
 
 from utils import constants
 from utils.command import build_analysis_command
-from utils.common import run_containerless_parametrize
+from utils.common import run_containerless_parametrize, verify_triggered_rule
 from utils.manage_maven_credentials import manage_credentials_in_maven_xml
 from utils.report import assert_story_points_from_report_file, get_json_from_report_output_file
 
@@ -29,7 +29,7 @@ def test_standard_analysis(app_name, analysis_data, additional_args):
     assert_story_points_from_report_file()
 
 
-def test_bug_3863_dependency_rule_analysis(analysis_data):
+def test_dependency_rule_analysis(analysis_data):
     application_data = analysis_data['tackle-testapp-project']
     custom_maven_settings = os.path.join(
         os.getenv(constants.PROJECT_PATH),
@@ -49,7 +49,7 @@ def test_bug_3863_dependency_rule_analysis(analysis_data):
         "",
         **{
             'maven-settings': custom_maven_settings,
-            'rules': custom_rule_path
+            'rules': custom_rule_path,
         }
     )
 
@@ -61,10 +61,4 @@ def test_bug_3863_dependency_rule_analysis(analysis_data):
 
     report_data = get_json_from_report_output_file()
 
-    ruleset = next((item for item in report_data['rulesets'] if item.get('description') == 'temp ruleset'), None)
-
-    assert ruleset is not None, "Ruleset property not found in output"
-    assert len(ruleset.get('skipped', [])) == 0, "Custom Rule was skipped"
-    assert len(ruleset.get('unmatched', [])) == 0, "Custom Rule was unmatched"
-    assert 'violations' in ruleset, "Custom rules didn't trigger any violation"
-    assert 'tackle-dependency-test-rule' in ruleset['violations'], "The test rule triggered no violations"
+    verify_triggered_rule(report_data, 'tackle-dependency-test-rule')
