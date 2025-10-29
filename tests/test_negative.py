4c0191ad0c2e3861cd6f66c29e7178b4616a5211
Alejandro Brugarolas <117646518+abrugaro@users.noreply.github.com>
2025-10-29 15:15:56 +0100

    automate invalid custom rule path test (#103)
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- tests/test_negative.py
+++ tests/test_negative.py
@@ -27,3 +27,29 @@ def test_analysis_wrong_target(analysis_data, additional_args):
     assert process.returncode != 0
 
     assert 'Error: unknown target:' in process.stderr
+
+# Automates Bug MTA-4951
+@run_containerless_parametrize
+def test_analysis_wrong_custom_rule(analysis_data, additional_args):
+    application_data = analysis_data['jee_example_app']
+
+    command = build_analysis_command(
+        application_data['file_name'],
+        application_data['sources'],
+        "",
+        **{'rules': "/an/invalid/path"}
+
+    )
+
+    process = subprocess.run(
+        command,
+        shell=True,
+        check=False,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding='utf-8'
+    )
+
+    assert process.returncode != 0
+
+    assert 'no such file or directory failed to stat rules at path' in process.stderr
